041c74428d7a937541aba67cb4b938485561ccd0
AlirezaDeris <abc@yahoo.com>
2023-09-17 22:21:42 +0330

    frontend:testCommit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,16 @@ import HomePage from "./Pages/HomePage/HomePage";
 import About from "./Pages/About/About";
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<About />} />
+            </Routes>
+
+            <h1>test</h1>
+        </>
+    );
 }
 
 export default App;
