d28456a978e9e03e4441542889e20281dd9de480
Matin <matinhosseiniofficial@gmail.com>
2025-05-04 20:55:55 +0330

    working on login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,79 +5,74 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
-// import {
-//   Form,
-//   FormControl,
-//   FormDescription,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-//   FormMessage,
-// } from '@/components/ui/form';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
+import { loginAction } from './_action';
 const formSchema = z.object({
-  username: z
-    .string()
+  phone_number: z
+    .string({
+      required_error: 'شماره موبایل الزامی است.',
+    })
     .min(2, 'نام کاربری می بایست حداقل 2 کاراکتر باشد.')
     .max(50),
 });
 
 export default function LoginPage() {
-  const {
-    register,
-    handleSubmit,
-    formState: { isSubmitting, errors },
-  } = useForm<z.infer<typeof formSchema>>({
+  const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
+    defaultValues: {
+      phone_number: '',
+    },
   });
 
   const onSubmit = async (data: any) => {
-    const res = await fetch('');
-    const datas = await res.json();
     console.log('form submitted', data);
+
+    const response = await loginAction(data);
+    console.log(response);
   };
 
   return (
     <>
-      {/* <Form {...form}>
+      <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-8 w-[500px] mx-auto"
+          className="w-[500px] mx-auto mt-10"
         >
           <FormField
             control={form.control}
-            name="username"
+            name="phone_number"
             render={({ field }) => (
-              <FormItem>
-                <FormLabel>نام کاربری</FormLabel>
+              <FormItem className="mb-2">
+                <FormLabel>شماره موبایل</FormLabel>
                 <FormControl>
-                  <Input placeholder="مثال: amir_ahmadi" {...field} />
+                  <Input placeholder="مثال: 09123456789" {...field} />
                 </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
                 <FormMessage />
               </FormItem>
             )}
           />
-          <Button type="submit">ثبت</Button>
+          <Button type="submit" className="w-full">
+            {form.formState.isSubmitting ? '......' : 'ادامه'}
+          </Button>
         </form>
-      </Form> */}
-
-      <form className=" w-[500px] mx-auto" onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="username">نام کاربری</label>
-        <Input
-          id="username"
-          {...register('username')}
-          placeholder="نام کاربری"
-        />
-        <p>{!!errors && errors.username?.message}</p>
-
-        {isSubmitting && <p>is submitting</p>}
-
-        <Button type="submit">ارسال</Button>
-      </form>
+      </Form>
+      <Button
+        onClick={() => {
+          window.open('google.com', '_blank');
+        }}
+      >
+        باز کردن صفحه جدید
+      </Button>
     </>
   );
 }
